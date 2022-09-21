9021753988eb0886ca03d2982dd3b477ac1f9f16
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-09-21 10:18:09 +0200

    fix style

--- database/migrations/2022_09_20_140000_add_agenda_column_to_stationary_events_table.php
+++ database/migrations/2022_09_20_140000_add_agenda_column_to_stationary_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddAgendaColumnToStationaryEventsTable  extends Migration
+class AddAgendaColumnToStationaryEventsTable extends Migration
 {
     public function up(): void
     {
