90858cfbb44bb7e5479b3e8be5b34fbe1615060d
dawid.miklas <dawid.miklas@escolasoft.com>
2023-05-19 09:51:07 +0200

    fix?

--- src/Repositories/StationaryEventRepository.php
+++ src/Repositories/StationaryEventRepository.php
@@ -48,15 +48,17 @@ class StationaryEventRepository extends BaseRepository implements StationaryEven
             ->orWhereHas('authors', function ($query) use ($userId) {
                 $query->where('author_id', $userId);
             })
-            ->select(StationaryEvent::TABLE_NAME . '.*', DB::raw("
-                (SELECT MAX(created_at) FROM (
-                    SELECT user_pivot.created_at FROM " . StationaryEventUserPivot::TABLE_NAME . " AS user_pivot
-                    WHERE user_pivot.stationary_event_id = " . StationaryEvent::TABLE_NAME . ".id AND user_pivot.user_id = $userId
-                    UNION ALL
-                    SELECT author_pivot.created_at FROM " . StationaryEventAuthorPivot::TABLE_NAME . " AS author_pivot
-                    WHERE author_pivot.stationary_event_id = " . StationaryEvent::TABLE_NAME . ".id AND author_pivot.author_id = $userId
-                ) AS x) AS max_created_at
-            "))
+            ->leftJoin(StationaryEventUserPivot::TABLE_NAME . ' as user_pivot', function ($join) use ($userId) {
+                $join->on('user_pivot.stationary_event_id', '=', StationaryEvent::TABLE_NAME . '.id')
+                    ->where('user_pivot.user_id', $userId);
+            })
+            ->leftJoin(StationaryEventAuthorPivot::TABLE_NAME . ' as author_pivot', function ($join) use ($userId) {
+                $join->on('author_pivot.stationary_event_id', '=', StationaryEvent::TABLE_NAME . '.id')
+                    ->where('author_pivot.author_id', $userId);
+            })
+            ->select(StationaryEvent::TABLE_NAME . '.*')
+            ->selectRaw('GREATEST(COALESCE(user_pivot.created_at, \'1970-01-01 00:00:00\'), COALESCE(author_pivot.created_at, \'1970-01-01 00:00:00\')) as max_created_at')
             ->orderBy('max_created_at', 'desc');
+
     }
 }
